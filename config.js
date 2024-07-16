b385faacd05d942aeb0de02b72718abf340ed5e6
Rushaun5 <166579083+Rushaun5@users.noreply.github.com>
2024-07-16 13:11:43 -0500

    Update config.js

--- config.js
+++ config.js
@@ -10,17 +10,17 @@ global.email = 'lazaromtaju@gmail.com'
 global.github = 'https://github.com/Lazack28/Lazack-md'
 global.location = 'Dodoma Tanzania'
 global.gurl = 'https://instagram.com/Lazack_28/' // add your username
-global.sudo = process.env.SUDO || "+255734980103"
-global.devs = '+255734980103'
+global.sudo = process.env.SUDO || "18762306464"
+global.devs = '18762306464'
 global.website = 'https://github.com/Lazack28/Lazack-md' //wa.me/+255734980103
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/2cbb7abbf988bf9e7a088.jpg'
 module.exports = {
-  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
+  sessionName: process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0V1KzVkUGpoU09LbDJ6ek5qVDRQSUtycWZnS3lXb2MxM2pYMys2eElWWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNThxTFJSTVVtcXduWDd2NHdkSnprUE1ybGJEM0hoTG92QUtsYnl4NkJ6VT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ1T2JiSmlWN3dKaTVFenlOR082dzl0eDUrdUNhYUlaMDN1S1NhcmJqdEUwPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ0dFBVTm9ubmg1QUJ3TTJzUHJRSHo4WWU2WE9URGMrWjVESXJTcDRGL1VBPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlNDQ0pvNFdXOFY5dW5yOFZiTk9VSjdRU1IrZGpweFdLOVpLM3FvWHExbVE9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InFGWjk3TW55bzB6WUdPcmN1aURGVUFIdEZzaERXZ3c0VlNrSnJ1N2R6WG89In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidUxTUDVQa3FsbFUxMU5ZQm80SndCdEJFb0hYYm1iS3ZXdUd3QnhuUGgwOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVzN6K0FmVGlvSFBhaXlZaGRVVlp2bng0Nm51dVBZeTdkVDYrclo0SUhDWT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkVNUlZCbEJVT1lMZlpyUXZnYkVUeFpmOVp3Z25SdDJhbkZ1dFc1US94M3MzSkpBSGpUVWM3UGFmT05XajdMQWtITlJ5MnJ4UnVuTTBtV1RZYmxocEF3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTIwLCJhZHZTZWNyZXRLZXkiOiJSY01VYVFPVi9DOGprTTVoNTMvY2dBVTI4YU8yUnlhR1lyVmZMclNWb2xNPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJub3I3MVBzRlRMS3YtUzFUU01yRHZRIiwicGhvbmVJZCI6ImNkYjFlOWRjLWU1NzMtNGYyMS05NjQ5LTBmYmUzZjNlODU0MiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ5bC94SVJ2dmtNOVVrZk5xWVhBT2NkdVFQaTg9In0sInJlZ2lzdGVyZWQiOmZhbHNlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlpzQjBib2R1cmc2cmNkV251MkhuTXljQXBFST0ifSwicmVnaXN0cmF0aW9uIjp7fSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0xXL3o5d0ZFUERxMnJRR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IkJvTUdORVFJNFc2aTBqUG5rT1V2REUwcE9OZTZENTRrNC9ENTN4bVNsaWc9IiwiYWNjb3VudFNpZ25hdHVyZSI6IlhkR2plSUNVdEc4TVBrem9GVk1WOG1hWjJpcll2TVBOSlF5YkZaMUhtcFNqN3ppRkMwT1pWc3Q0VVQ5RFdrTnhTbndMQWdTOTlheS9pYis0NDdUMUN3PT0iLCJkZXZpY2VTaWduYXR1cmUiOiJTQXVtb1ZLZW9vUDFoZ3BiTGtjNlUxVm90bGRUaDdqNWJscGtqV0E5dC91Z3V3Z0lsZ2FkWEFiNTViSEx6QjlGMVFUcG9QV0hFRXhDbGowY1NmMlBEUT09In0sIm1lIjp7ImlkIjoiMTg3NjIzMDY0NjQ6MTBAcy53aGF0c2FwcC5uZXQifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMTg3NjIzMDY0NjQ6MTBAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCUWFEQmpSRUNPRnVvdEl6NTVEbEx3eE5LVGpYdWcrZUpPUHcrZDhaa3BZbyJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyMTE1Mjg5NH0=",      //Put Your Session Id Here
   author:  process.env.PACK_AUTHER ||  'LAZACK BOT',
   packname:  process.env.PACK_NAME || 'CREDIT TO LAZACK',
   
-  botname:   process.env.BOT_NAME === undefined ? "LAZACK-MD" : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? '𝙇𝘼𝙕𝘼𝘾𝙆-𝙈𝘿' : process.env.OWNER_NAME,  
+  botname:   process.env.BOT_NAME === undefined ? "Patrickbot" : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'Shaun' : process.env.OWNER_NAME,  
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS,
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
@@ -28,7 +28,7 @@ module.exports = {
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '234' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? true : process.env.AUTO_STATUS_SAVER,
-  HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
+  HANDLERS:  process.env.PREFIX === undefined ? '-' : process.env.PREFIX,
   warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
   disablepm:  process.env.DISABLE_PM === undefined ? true : process.env.DISABLE_PM,
   levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
